9109f68c169852fea8ac43ae099297ac3a2faf1c
Julien <julien.tavet30@gmail.com>
2021-04-06 10:28:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 const data = {
-  soundbox1: {
-    E: { sound: "media/soundskit/808/8081.wav" },
-    R: { sound: "media/soundskit/Chant/chant.wav" },
-    T: { sound: "media/soundskit/Clap/Clap.wav" },
-    D: { sound: "media/soundskit/HiHatAndMore/Hihat.wav" },
-    F: { sound: "media/soundskit/Kick/kick.wav" },
-    G: { sound: "media/soundskit/Snap/snap.wav" },
-    C: { sound: "media/soundskit/Snare/snare2.wav" },
-    V: { sound: "media/soundskit/HiHatAndMore/cowbell.wav" },
-    B: { sound: "media/soundskit/HiHatAndMore/cymbal.wav" },
-  },
+  E: { sound: "media/soundskit/808/8081.wav" },
+  R: { sound: "media/soundskit/Chant/chant.wav" },
+  T: { sound: "media/soundskit/Clap/Clap.wav" },
+  D: { sound: "media/soundskit/HiHatAndMore/Hihat.wav" },
+  F: { sound: "media/soundskit/Kick/kick.wav" },
+  G: { sound: "media/soundskit/Snap/snap.wav" },
+  C: { sound: "media/soundskit/Snare/snare2.wav" },
+  V: { sound: "media/soundskit/HiHatAndMore/cowbell.wav" },
+  B: { sound: "media/soundskit/HiHatAndMore/cymbal.wav" },
 };
 const construct = () => {
-  for (const letter in data.soundbox1) {
+  for (const letter in data) {
     const keyDiv = document.createElement("div");
     keyDiv.classList.add("drum");
+
     let h2 = document.createElement("h2");
     h2.textContent = letter;
-    h2.classList.add("soundkey");
+
     keyDiv.appendChild(h2);
+
     document.getElementById("musicBox").appendChild(keyDiv);
-    let icon = document.createElement("i");
-    icon.classList.add("fas");
-    icon.classList.add("fa-music");
-    h2.appendChild(icon);
+
     keyDiv.addEventListener("click", (e) => {
       let letter = e.currentTarget.querySelector("h2").textContent;
       playDrum(letter);
@@ -33,7 +30,7 @@ const construct = () => {
 
 const playDrum = (letter) => {
   const audio = new Audio();
-  audio.src = data.soundbox1[letter];
+  audio.src = data[letter].sound;
   console.log;
   audio.play();
 };
